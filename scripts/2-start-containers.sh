4f298b871edb5d5905965f71e2e6805045d5422c
vinh doan ngoc <doanngocvinh1@gmail.com>
2023-12-19 18:32:20 +0700

    .

--- scripts/2-start-containers.sh
+++ scripts/2-start-containers.sh
@@ -10,7 +10,9 @@ docker run -itd \
            --name hadoop-master \
            --hostname hadoop-master \
            docker_hadoop 
-docker run -itd --net=hadoop --name hadoop-slave1 --hostname hadoop-slave2 docker_hadoop
+docker run -itd --net=hadoop --name hadoop-slave1 --hostname hadoop-slave1 docker_hadoop
+docker run -itd --net=hadoop --name hadoop-slave2 --hostname hadoop-slave2 docker_hadoop
+
 
 docker exec hadoop-master sh -c "hdfs namenode -format"
 docker exec -it namenode bash
