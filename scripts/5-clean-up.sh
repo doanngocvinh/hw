9f05a03dbf74e4be63522ba146e82533b0758505
vinh doan ngoc <doanngocvinh1@gmail.com>
2023-12-19 17:09:48 +0700

    .

--- scripts/5-clean-up.sh
+++ scripts/5-clean-up.sh
@@ -1,6 +1,6 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q) #remove all dangling containers
 # docker rmi $(docker images -f "dangling=true" -q)
-docker rmi hadoop_base
+docker rmi docker_hadoop
 # A handy script to remove all unused/dangling images and containers.
-docker network rm -f dock_net
+docker network rm -f hadoop
